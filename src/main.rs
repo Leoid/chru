5d694939a8c5eaa588f2c37e6e8c614ad771fa33
Leoid <hmali201488@gmail.com>
2020-09-13 11:43:45 -0400

    Improving the crawler

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,15 @@ use std::fs::File;
 use std::io;
 use std::path::Path;
 
+
+/// Link Options Enum
 enum LinkOptions{
     INTERNAL,
     EXTERNAL,
     ALL,
 }
 
+/// Reading lines from a file
 fn read_lines(path: &str) -> std::io::Result<Vec<String>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
@@ -47,6 +50,7 @@ fn main(){
     //get_urls("http://b1twis3.ca/burpsuite-30-pro-tips/");
 }
 
+/// Fetch URLs based on `LinkOptions` and save them into `fetched_urls` vector
 #[tokio::main]
 async fn get_urls(option: LinkOptions,fetched_urls: &mut Vec<String>,_url: &str) -> Result<(), Box<dyn std::error::Error>> {
     // Start Scraping
@@ -64,16 +68,46 @@ async fn get_urls(option: LinkOptions,fetched_urls: &mut Vec<String>,_url: &str)
 
     let body = resp;
     let fragment = Html::parse_document(&body);
-    let selector = Selector::parse("a").unwrap();
 
-    let mut urls = Vec::new();
-    for element in fragment.select(&selector){
-        //println!("[ULR]: {}",element.value().attr("href").unwrap());
-        urls.push(element.value().attr("href").unwrap());
-    }
+    // Selector & Element
+    let target_tags = vec!["a","link","script","img"];
+    //let mut urls = Vec::new();
+
+    let mut urls: Vec<_> = target_tags.into_iter().map( |tag| {
+        let selector = Selector::parse(tag).unwrap();
+
+        for element in fragment.select(&selector){
+            match tag {
+               "a" => {
+                    //element.value().attr("href").unwrap();
+                    println!("[a]: {}",element.value().attr("href").unwrap());
+                }
+
+               "link" => {
+                   println!("[href]: {}",element.value().attr("href").unwrap());
+               }
+
+               "script" => {
+                   println!("[SRC]: {}",element.value().attr("src").unwrap());
+               }
+               "img" => {
+                   println!("[IMG]: {}",element.value().attr("src").unwrap());
+               }
+               _ => {
+
+               }
+            }
+
+        }
+
+    }).collect();
+
+
+    //-- End of Selector
 
     // Cleaning the URLs vector
     let mut urls: Vec<_> = urls.into_iter().unique().collect();
+    /*
     for i in urls{
 
 
@@ -137,6 +171,7 @@ async fn get_urls(option: LinkOptions,fetched_urls: &mut Vec<String>,_url: &str)
             }
         }
     }
+*/
 
     Ok(())
 
